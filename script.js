c0112598458ad74951bab8666873da5266a571aa
priteshvyas <priteshvyas0809@gmail.com>
2024-06-18 10:41:53 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+const btn = document.getElementById("btn")
+const birthday = document.getElementById("birthday");
+const result = document.getElementById("result");
+
+
+function calculateAge() {
+    const birthdayValue = birthday.value
+    if (birthdayValue == '') {
+        alert("please enter your birthday")
+    } else {
+        const age = getAge(birthdayValue); // Passed birthdayValue to getAge function
+        result.innerText = `Your age is ${age} ${age > 1 ? "years" : "year"} old`;
+    }
+
+}
+
+function getAge(birthdayValue) {
+    const currentDate = new Date()
+
+    const birthdayDate = new Date(birthdayValue);
+    let age = currentDate.getFullYear() - birthdayDate.getFullYear()
+    const month = currentDate.getMonth() - birthdayDate.getMonth()
+
+    if (month < 0 || (month === 0 && currentDate.getDate() < birthdayDate.getDate())) {
+        age--;
+
+    }
+    return age;
+
+
+}
+
+btn.addEventListener("click", calculateAge);
